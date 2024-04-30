48ce50aeab9c531c78ce5550eef57a15b4c558c2
Nicola Spadari <NicolaSpadari@users.noreply.github.com>
2024-04-30 17:47:01 +0200

    Push debug gradient

--- composables/gradient.ts
+++ composables/gradient.ts
@@ -7,26 +7,33 @@ export const useGradient = (section: string) => {
 	const indigo = typeof colors?.indigo === "string" ? colors?.indigo : colors?.indigo?.[50];
 	const orange = typeof colors?.orange === "string" ? colors?.orange : colors?.orange?.[50];
 
+	console.log(lime, cyan, pink, indigo, orange);
+
 	const tl = useGsap.timeline({ repeat: -1 });
 
 	tl.to(section, {
+		backgroundColor: lime,
+		duration: 3,
+		onComplete: () => console.log("lime completed")
+	});
+	tl.to(section, {
 		backgroundColor: cyan,
-		duration: 3
+		duration: 3,
+		onComplete: () => console.log("cyan completed")
 	});
 	tl.to(section, {
 		backgroundColor: pink,
-		duration: 3
+		duration: 3,
+		onComplete: () => console.log("pink completed")
 	});
 	tl.to(section, {
 		backgroundColor: indigo,
-		duration: 3
+		duration: 3,
+		onComplete: () => console.log("indigo completed")
 	});
 	tl.to(section, {
 		backgroundColor: orange,
-		duration: 3
-	});
-	tl.to(section, {
-		backgroundColor: lime,
-		duration: 3
+		duration: 3,
+		onComplete: () => console.log("orange completed")
 	});
 };
