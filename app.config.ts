eefc3eca427ce70b770b4405b1e736e2001e1280
Nicola Spadari <NicolaSpadari@users.noreply.github.com>
2024-11-27 11:07:11 +0100

    Update deps

---
+++ app.config.ts
@@ -0,0 +1,23 @@
+export default defineAppConfig({
+	user: {
+		name: "Nicola",
+		surname: "Spadari",
+		email: "spadari.nicola@gmail.com",
+	},
+	socials: {
+		github: {
+			icon: "carbon:logo-github",
+			id: "github",
+			label: "GitHub",
+			link: "https://github.com/NicolaSpadari"
+		},
+		linkedin: {
+			icon: "carbon:logo-linkedin",
+			id: "linkedin",
+			label: "LinkedIn",
+			link: "https://linkedin.com/in/nicolaspadari"
+		}
+	},
+	currentYear: new Date().getFullYear(),
+	upToDesktopMediaQuery: "(min-width: 992px)",
+});
